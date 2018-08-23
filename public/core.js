16e3716bf6050b22d25f7749781c2350000469ef
Lucy <28128984+lucylin43@users.noreply.github.com>
2018-08-23 16:44:36 -0700

    fix home.html view's controller to reduce redundancy

--- public/core.js
+++ public/core.js
@@ -25,16 +25,53 @@ scotchTodo.config(function($routeProvider) {
 });
 
 // create the controller and inject Angular's $scope
-scotchTodo.controller('mainController', function($scope) {
+scotchTodo.controller('mainController', function($scope, $http) {
     // create a message to display in our view
-    $scope.message = 'home page is me!';
+    $scope.message = 'Welcome to the Home Page!';
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
 });
 
 scotchTodo.controller('aboutController', function($scope) {
-    $scope.message = 'about page is me!';
+    $scope.message = 'Welcome to the About Page!';
 });
 
 scotchTodo.controller('contactController', function($scope) {
-    $scope.message = 'contact page is me!';
+    $scope.message = 'Welcome to the Contact Page!';
 });
 
--- server.js
+++ server.js
@@ -84,7 +84,7 @@
 
     // application -------------------------------------------------------------
      app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 
     // listen (start app with node server.js) ======================================
